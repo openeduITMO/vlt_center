4160789c28e203a209463ec9c0de50a4795bbda0
Дмитрий <dmi.nichik@gmail.com>
2016-04-24 05:01:34 +0300

    new structure

--- src/main/java/com/spring/boot/vlt/VltApplication.java
+++ src/main/java/com/spring/boot/vlt/VltApplication.java
@@ -25,7 +25,6 @@ public class VltApplication extends WebMvcConfigurerAdapter {
 
     @Override
         public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
         registry.addResourceHandler("/vlabs/**").addResourceLocations("/VLabs/");
     }
 
@@ -33,7 +32,7 @@ public class VltApplication extends WebMvcConfigurerAdapter {
     public ITemplateResolver defaultTemplateResolver() {
         TemplateResolver resolver = new FileTemplateResolver();
         resolver.setSuffix(".html");
-        resolver.setPrefix("src/main/webapp/WEB-INF/view/");
+        resolver.setPrefix("src/main/resources/templates/");
         resolver.setTemplateMode("HTML5");
         resolver.setCharacterEncoding("UTF-8");
         resolver.setCacheable(false);
